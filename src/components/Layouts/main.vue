d38d50332c8f059f82d307432c0352c23918754e
Andersio Lopes <billyscouf@gmail.com>
2024-08-07 10:03:56 +0100

    primeiro commit

---
+++ src/components/Layouts/main.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <main>
+        <h1>Hello World!</h1>
+        <p>Este é o meu primeiro projeto em Vue Js</p>
+    </main>
+</template>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: 0;
+        font-family: sans-serif;
+    }
+    body{
+        width: 100%;
+        height: 100dvh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
